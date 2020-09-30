e081af3309e07164d926a492010767d3fe2fbd7f
邓江洲 <707703386@qq.com>
2020-09-30 15:34:43 +0800

    add folder

--- project/socketG/socketG/Ctrls/FileAndMusic/MusicBroswer.swift
+++ project/socketG/socketG/Ctrls/FileAndMusic/MusicBroswer.swift
@@ -14,6 +14,14 @@ protocol MusicBroswerDelegate: class{
 }
 
 
+enum SourceOption{
+    case file, folder
+    
+    
+    static let row = "row"
+    static let kind = "kind"
+}
+
 class MusicBroswer: NSViewController {
     
     weak var delegate: MusicBroswerDelegate?
@@ -21,7 +29,7 @@ class MusicBroswer: NSViewController {
     @IBOutlet weak var table: NSTableView!
     
     var files = [URL]()
-    
+    var folders = [URL]()
     
     
 
@@ -34,7 +42,16 @@ class MusicBroswer: NSViewController {
         view.frame = NSRect(x: 0, y: 0, width: 500, height: 400)
         table.delegate = self
         table.dataSource = self
-        NotificationCenter.default.addObserver(self, selector: #selector(MusicBroswer.didSelectRow(_:)), name: NSTableView.selectionDidChangeNotification, object: table)
+        var row = table.selectedRow
+        var kind = SourceOption.file
+        if row >= folders.count{
+            row -= folders.count
+            kind = .file
+        }
+        
+        let info: [String : Any] = [SourceOption.row: row,
+                                    SourceOption.kind: kind]
+        NotificationCenter.default.addObserver(self, selector: #selector(MusicBroswer.didSelectRow(_:)), name: NSTableView.selectionDidChangeNotification, object: info)
         
         
     }
@@ -50,7 +67,9 @@ class MusicBroswer: NSViewController {
                 for url in paths{
                     let isDirectory = (try url.resourceValues(forKeys: [.isDirectoryKey])).isDirectory ?? false
                     let musicExtern = ["mp3", "m4a"]
-                    if isDirectory == false, musicExtern.contains(url.pathExtension){
+                    if isDirectory{
+                        folders.append(url)
+                    } else if musicExtern.contains(url.pathExtension){
                         files.append(url)
                     }
                 }
@@ -65,12 +84,17 @@ class MusicBroswer: NSViewController {
     
     @objc
     func didSelectRow(_ noti: Notification){
-        guard let table = noti.object as? NSTableView else {
+        guard let table = noti.object as? [String : Any], let row = table[SourceOption.row] as? Int, let kind = table[SourceOption.kind] as? SourceOption else {
             return
         }
-        let row = table.selectedRow
         assert(row >= 0, "不能选个负数")
-        delegate?.didSend(data: files[row])
+        switch kind {
+        case .file:
+            delegate?.didSend(data: files[row])
+        case .folder:
+            ()
+        }
+        
         
         
         
@@ -101,7 +125,7 @@ class MusicBroswer: NSViewController {
 
 extension MusicBroswer: NSTableViewDataSource{
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return files.count
+        return folders.count + files.count
     }
     
     
@@ -111,21 +135,41 @@ extension MusicBroswer: NSTableViewDataSource{
 
 extension MusicBroswer: NSTableViewDelegate{
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-       
-        let cell = NSTextField()
-        cell.isEditable = false
-        //  let cell = tableView.makeView(withIdentifier: .contentFile, owner: self) as! NSTableCellView
-        let attribute = [NSAttributedString.Key.foregroundColor: NSColor.red, NSAttributedString.Key.font: NSFont.systemFont(ofSize: 25)]
-        let attrString = NSAttributedString(string: files[row].lastPathComponent, attributes: attribute)
-        cell.attributedStringValue = attrString
-        //  cell.textField?.attributedStringValue = attrString
-        return cell
+        let folderCount = folders.count
+        if row < folderCount{
+            let cell = NSTextField()
+            cell.isEditable = false
+          
+            let attribute = [NSAttributedString.Key.foregroundColor: NSColor.green, NSAttributedString.Key.font: NSFont.systemFont(ofSize: 25)]
+            let info = folders[row].lastPathComponent + "    >"
+            let attrString = NSAttributedString(string: info, attributes: attribute)
+            cell.attributedStringValue = attrString
+          
+            return cell
+        }
+        else{
+            let item = row - folderCount
+            let cell = NSTextField()
+            cell.isEditable = false
+           
+            let attribute = [NSAttributedString.Key.foregroundColor: NSColor.red, NSAttributedString.Key.font: NSFont.systemFont(ofSize: 25)]
+            let attrString = NSAttributedString(string: files[item].lastPathComponent, attributes: attribute)
+            cell.attributedStringValue = attrString
+            return cell
+        }
     }
     
     
     
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        return 40
+        if row < folders.count{
+            return 70
+            
+        }
+        else{
+            return 40
+        }
+        
     }
     
     
